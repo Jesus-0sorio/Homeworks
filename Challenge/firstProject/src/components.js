cb01b9237d83afe7ae65fecbe0ab680dfe48606b
Jesus-0sorio <jaoz10.16@gmail.com>
2023-02-20 14:34:54 -0500

    display all components and file import components

---
+++ Challenge/firstProject/src/components.js
@@ -0,0 +1,4 @@
+export {default as FirstApp} from './components/FirstApp' 
+export {default as Counter} from './components/Counter' 
+export {default as ComponentApp} from './components/ComponentApp' 
+export {default as GifExpertApp} from './components/GifExpertApp' 
