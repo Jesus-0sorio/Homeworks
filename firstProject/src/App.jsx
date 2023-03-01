31591f6bc3476e5e739c86fe4002f6f3a8d8abaa
Jesus-0sorio <jaoz10.16@gmail.com>
2023-02-28 23:31:58 -0500

    😾

--- firstProject/src/App.jsx
+++ firstProject/src/App.jsx
@@ -1,39 +1,8 @@
 import { useState } from "react";
-import { FirstApp, Counter, ComponentApp, GifExpertApp, Gifs, CounterHook, BreakingBad } from "./components";
+import { ComponentApp } from "./components";
 
-const arrayComponent = [
-  <FirstApp />,
-  <Counter value={0} />,
-  <ComponentApp />,
-  <GifExpertApp />,
-  <Gifs />,
-  <CounterHook value={0}/>,
-  <BreakingBad />
-];
+import "./App.css";
 
 export function App() {
-  const [component, setComponent] = useState(0);
-
-  const changeComponent = (e) => {
-    const result = e.target.id === "plus" ? component + 1 : component - 1;
-    const size = arrayComponent.length - 1;
-    return result > size
-      ? setComponent(0)
-      : result < 0
-      ? setComponent(size)
-      : setComponent(result);
-  };
-
-  return (
-    <>
-      <h1>Challenge {component + 1}</h1>
-      {arrayComponent[component]}
-      <div>
-        <button onClick={changeComponent}>{"<"}</button>
-        <button onClick={changeComponent} id="plus">
-          {">"}
-        </button>
-      </div>
-    </>
-  );
+  return <ComponentApp />;
 }
