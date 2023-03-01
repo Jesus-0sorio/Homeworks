3fc436b7e7e00d8bbf1ea2a87f43bce0f5a452b1
Jesus-0sorio <jaoz10.16@gmail.com>
2023-02-28 23:33:32 -0500

    😾

--- firstProject/src/components.js
+++ firstProject/src/components.js
@@ -1,7 +1,2 @@
-export { default as FirstApp } from "./components/Challenge-1/FirstApp";
-export { default as Counter } from "./components/Challenge-2/Counter";
-export { default as ComponentApp } from "./components/Challenge-3/ComponentApp";
-export { default as GifExpertApp } from "./components/Challenge-4/GifExpertApp";
-export { default as Gifs } from "./components/Challenge-5/Gifs";
-export { default as CounterHook } from "./components/Challenge-6/CounterHook";
-export { BreakingBad } from "./components/Challenge-8/BreakingBad";
+
+export { default as Counter } from "./components/Counter";
